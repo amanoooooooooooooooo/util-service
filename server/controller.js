89109f8820bacaaeaae87064fa91c7bc9befa6f9
amanoooo <amanoooooooooooooooo@gmail.com>
2019-02-12 13:58:52 +0800

    add webhook

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,15 @@
+var fetch = require('isomorphic-unfetch')
+
 const addControllers = (server) => {
   server.get('/header', (req, res) => {
     res.json(req.headers)
+  })
+
+  server.get('/utilwebhook', async (req, res) => {
+    const resp = await fetch('http://fe2o3.club:9000/api/webhooks/376fc971-5cdf-4bcb-9a0d-78c9bdd12b1a', {
+      method: 'POST'
+    }).then((res) => res.text())
+    res.json(resp)
   })
 }
 module.exports = {
