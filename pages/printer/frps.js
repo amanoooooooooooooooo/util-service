58f60eb6f652e2cceb4507b5d82e8e2ca05aa9db
amanoooo <amanoooooooooooooooo@gmail.com>
2019-03-07 14:48:46 +0800

    update frps

--- pages/printer/frps.js
+++ pages/printer/frps.js
@@ -1,30 +1,25 @@
 import React from 'react'
 import Layout from '../../components/MyLayout.js'
-import fetch from 'isomorphic-unfetch'
-
-import dynamic from 'next/dynamic'
-const DynamicReactJson = dynamic(import('react-json-view'), { ssr: false })
 
 class PortForwarding extends React.Component {
-    state = {
-      domain: '',
-      ip: '39.104.226.149',
-      response: {},
-      domainList: []
-    }
-    render () {
-      return <Layout>
-        <h2>Public frp server</h2>
-        <h3>Ip: 39.104.226.149</h3>
-        <pre>
-        #frps.ini<br />
-        [common]<br />
-        bind_port = 7000<br />
-        vhost_http_port = 7001<br />
-        </pre>
+  render () {
+    return <Layout>
+      <h2>Public frp server</h2>
+      <h3>Public ip: 39.104.226.149</h3>
+      <pre>
+            cat > frps.ini &lt;&lt; EOF<br />
+            # frps.ini <br />
+        <br />
+            [common]<br />
+            bind_port = 7000<br />
+            vhost_http_port = 7001<br />
+            EOF<br />
+        <br />
+docker run -d -p 7000:7000 -p 7001:7001 -v $(pwd)/frps.ini:/conf/frps.ini amanohikaru/frp-docker
+      </pre>
 
-      </Layout>
-    }
+    </Layout>
+  }
 }
 
 export default PortForwarding
