570e6409a5e5d849776a450986e79cd331e54a82
hikaru amano <amanoooooooooooooooo@gmail.com>
2019-08-07 16:28:50 +0800

    fix build

--- pages/setting.tsx
+++ pages/setting.tsx
@@ -1,15 +1,18 @@
 import Layout from '../components/MyLayout'
 
 import dynamic from 'next/dynamic'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { delCookie, setCookie, getCookie } from '../utils';
 import { LOCAL } from '../client/constant';
 const Color = dynamic(import('../components/Color'), { ssr: false })
 
 function Setting() {
-    const _mode = getCookie(LOCAL.MODE_DEV) || '0'
-    const [model, setModel] = useState(parseInt(_mode))
+    const [model, setModel] = useState(0)
 
+    useEffect(() => {
+        const _mode = getCookie(LOCAL.MODE_DEV) || '0'
+        setModel(parseInt(_mode))
+    })
     const _toggleDev = () => {
         if (model) {
             delCookie(LOCAL.MODE_DEV)
